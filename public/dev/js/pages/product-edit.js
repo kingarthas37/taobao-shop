9079a09ba51ddcd6c28ba1f8b758fd1a0c50e5a6
ralf <kingarthas37@hotmail.com>
2015-11-09 16:49:50 +0800

    git

--- public/dev/js/pages/product-edit.js
+++ public/dev/js/pages/product-edit.js
@@ -1,15 +1,63 @@
 'use strict';
 
 module.exports = function () {
+    
+    
+    //使用购买说明信息
+    (function() {
+    
+        var contentInfo = $('#content-info');
+        var mdCodeInfo = $('#md-code-info');
+
+        mdCodeInfo.val('![]('+ contentInfo.find('.product-title img').attr('src') +')');
+
+        contentInfo.find(':checkbox').click(function() {
+            if(this.checked) {
+                mdCodeInfo.val('![]('+ contentInfo.find('.product-title img').attr('src') +')');
+            } else {
+                mdCodeInfo.val('');
+            }
+        });
+    
+    })();
+    
+    
     //选择banner
     (function() {
 
         var select = $('#select-banner');
+        var bannerLength = select.find('option').length - 1;
+        var codeBanner = $('.code-banner');
+        var mdCodeBanner = $('#md-code-banner');
+        
+        var count = Math.floor(Math.random(100) * bannerLength);
+        var currentBannerSrc = select.find('option:eq(' + count + ')').attr('data-src');
+        var currentBannerTitle = select.find('option:eq(' + count + ')').text();
 
+        codeBanner.html('<img src="'+ currentBannerSrc +'"/>');
+        mdCodeBanner.val('!['+ currentBannerTitle +']('+ currentBannerSrc +')');
 
-        console.info('abc');
+        select.on('change',function() {
+            
+            if(parseInt(this.value) === 0) {
+                var count = Math.floor(Math.random(100) * bannerLength);
+                currentBannerSrc = select.find('option:eq(' + count + ')').attr('data-src');
+                currentBannerTitle = select.find('option:eq(' + count + ')').text();
+            } else {
+                currentBannerSrc = select.find('option:eq(' + this.value + ')').attr('data-src');
+                currentBannerTitle = select.find('option:eq(' + this.value + ')').text();
+            }
+            codeBanner.html('<img src="'+ currentBannerSrc +'"/>');
+            mdCodeBanner.val('!['+ currentBannerTitle +']('+ currentBannerSrc +')');
+
+        });
 
     })();
     
     
-}
+    
+    $('.am-form').submit(function() {
+        
+    });
+    
+};
--- routes/product-edit.js
+++ routes/product-edit.js
@@ -3,60 +3,37 @@
 var router = require('express').Router();
 var AV = require('leanengine');
 
+var markdown = require( "markdown" ).markdown;
 
-var Todo = AV.Object.extend('Todo');
 
-// 查询 Todo 列表
 router.get('/', function(req, res, next) {
     
     return res.render('product-edit',{
-        name:"111"
+        name:"111",
+        md:'kingarthas'
     });
     
-    var query = new AV.Query(Todo);
-    query.descending('createdAt');
-    query.find({
-        success: function(results) {
-            res.render('todos', {
-                title: 'TODO 列表',
-                todos: results
-            });
-        },
-        error: function(err) {
-            if (err.code === 101) {
-                // 该错误的信息为：{ code: 101, message: 'Class or object doesn\'t exists.' }，说明 Todo 数据表还未创建，所以返回空的 Todo 列表。
-                // 具体的错误代码详见：https://leancloud.cn/docs/error_code.html
-                res.render('todos', {
-                    title: 'TODO 列表',
-                    todos: []
-                });
-            } else {
-                next(err);
-            }
-        }
-    });
+   
 });
 
 // 新增 Todo 项目
 router.post('/', function(req, res, next) {
+
+    
+    var mdContent = req.body['md-code-review'];
+    
+    
+    var md = markdown.toHTML( mdContent );
+    
     
     
     return res.render('product-edit',{
-        name:"kingarthas"
+        name:"kingarthas",
+        md:md
     });
     
     
-    var content = req.body.content;
-    var todo = new Todo();
-    todo.set('content', content);
-    todo.save(null, {
-        success: function(todo) {
-            res.redirect('/todos');
-        },
-        error: function(err) {
-            next(err);
-        }
-    })
-})
+    
+});
 
 module.exports = router;
